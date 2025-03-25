5095abd9a0b3de802f9d26241ab0cd694893b987
lsafer-meemer <135058778+lsafer-meemer@users.noreply.github.com>
2025-03-25 15:36:47 +0300

    feat(storage): added select(...)

--- sundry-storage/src/commonMain/kotlin/SimpleDataStore.kt
+++ sundry-storage/src/commonMain/kotlin/SimpleDataStore.kt
@@ -1,9 +1,14 @@
 package net.lsafer.sundry.storage
 
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.decodeFromJsonElement
 
 class SimpleDataStore<T : Any>(initial: T) {
     val data = MutableStateFlow(initial)
@@ -12,3 +17,21 @@ class SimpleDataStore<T : Any>(initial: T) {
 fun SimpleDataStore<JsonObject>.edit(
     block: (MutableMap<String, JsonElement>) -> Unit,
 ) = data.update { JsonObject(buildMap { putAll(it); block(this) }) }
+
+inline fun <reified T : Any> SimpleDataStore<JsonObject>.select(key: String, json: Json = Json): Flow<T?> {
+    return data.map {
+        val value = it[key] ?: return@map null
+        safeSerialTryOrNull { json.decodeFromJsonElement<T>(value) }
+    }
+}
+
+@PublishedApi
+internal inline fun <R> safeSerialTryOrNull(action: () -> R): R? {
+    return try {
+        action()
+    } catch (_: SerializationException) {
+        null
+    } catch (_: IllegalArgumentException) {
+        null
+    }
+}
