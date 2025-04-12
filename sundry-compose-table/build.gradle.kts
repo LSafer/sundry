0594b174a7199b1cc1b8422dfa01045abc4af458
lsafer-meemer <135058778+lsafer-meemer@users.noreply.github.com>
2025-04-12 17:22:02 +0300

    build: jvm target 17 fix

--- sundry-compose-table/build.gradle.kts
+++ sundry-compose-table/build.gradle.kts
@@ -70,7 +70,7 @@ android {
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         compose = true
